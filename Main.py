3b868023b01b4b04508f5134a1af03ccf0efdff2
Smint <iqqyu155@gmail.com>
2025-07-24 06:38:45 +0900

    DB 클래스 추가 및 MongoDB 연결 로직 통합, Bot 클래스에서 DB 인스턴스 초기화

--- Main.py
+++ Main.py
@@ -1,16 +1,30 @@
 import discord, os
 from discord.ext import commands
 from discord import Intents
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 
-
+class DB:
+    Discord = None
+    def __init__(self, MONGO_URI):
+        self.Discord = MongoClient(MONGO_URI, server_api=ServerApi('1'))["Discord"]
+    
+    def whlog(self):
+        '''
+        유저가 등록과정을 거쳤는지 확인합니다.
+        즉, 해당 id를 가진 사람이 DB에 속해있는지 확인합니다.
+        반환 값은 그 id를 가진 플레이어의 DB입니다.
+        '''
 
 class Bot(commands.Bot):
-    def __init__(self):
+    DB = None
+    def __init__(self, MONGO_URI):
         super().__init__(
             command_prefix="/",
             intents = discord.Intents.all()
         )
+        self.DB = DB(MONGO_URI)
         
     async def startup(self):
         await self.wait_until_ready()
@@ -54,8 +68,8 @@ class Bot(commands.Bot):
     
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-    
+MONGO_URI = os.getenv("MONGO_URI")
 intents = Intents()
-bot = Bot()
+bot = Bot(MONGO_URI)
 bot.run(DISCORD_TOKEN)
 
--- cogs/cog.py
+++ cogs/cog.py
@@ -1,32 +1,17 @@
 import discord, datetime, os
 from discord.ext import commands
 from discord.app_commands import Group
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
+import Main
 
-class DB:
-    Discord = None
-    def __init__(self):
-        load_dotenv()
-        MONGO_URI = os.getenv("MONGO_URI")
-        print(MONGO_URI)
-        self.Discord = MongoClient(MONGO_URI, server_api=ServerApi('1'))["Discord"]
-        print("MongoDB에 연결!")
-    def whlog(self):
-        '''
-        유저가 등록과정을 거쳤는지 확인합니다.
-        즉, 해당 id를 가진 사람이 DB에 속해있는지 확인합니다.
-        반환 값은 그 id를 가진 플레이어의 DB입니다.
-        '''
 
 class money(commands.Cog):
     util_group = Group(name="money", description="돈에 관련된 명령어들을 지원합니다.")
+    def __init__(self, bot: Main.Bot):
+        self.Dbcord = bot.DB
         
     @util_group.command(description="마치 노숙자처럼 바닥에 돈이 있는지 살펴봅니다.")
     async def search(self, interaction: discord.Interaction):
-        Dbcord = DB()
-        collection = Dbcord.Discord["User"]
+        collection = self.Dbcord.Discord["User"]
         user_id = interaction.user.id
         if collection.count_documents({"name" : user_id}):
             if (datetime.datetime.now() - collection.find_one({"name" : user_id})["cooltime"]).total_seconds() >= 300:
@@ -47,11 +32,12 @@ class money(commands.Cog):
 
 class status(commands.Cog):
     util_group = Group(name="status", description="자신의 정보를 확인합니다.")
+    def __init__(self, bot: Main.Bot):
+        self.Dbcord = bot.DB
         
     @util_group.command(description="전체적인 정보를 확인합니다.")
     async def all(self, interaction: discord.Interaction):
-        Dbcord = DB()
-        collection = Dbcord.Discord["User"]
+        collection = self.Dbcord.Discord["User"]
         user = interaction.user
         embed = discord.Embed(title=f"{user.name}", colour=0x00ff00)
         embed.add_field(name="계정 생성 날짜", value=f"{user.created_at.year}.{user.created_at.month}.{user.created_at.day}", inline=False)
@@ -78,7 +64,7 @@ class reload(commands.Cog):
             await interaction.response.send_message(f"오류 내용 : {e}")
             print("Fail..")
 
-async def setup(bot: commands.Bot):
-    await bot.add_cog(money())
-    await bot.add_cog(status())
+async def setup(bot: Main.Bot):
+    await bot.add_cog(money(bot))
+    await bot.add_cog(status(bot))
     await bot.add_cog(reload(bot))
